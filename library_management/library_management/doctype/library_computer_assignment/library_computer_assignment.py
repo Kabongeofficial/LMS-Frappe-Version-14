bbca56d7b570374cbc61ff9043aac932406bd190
kabongeofficial <musakabonge01@gmail.com>
2023-10-27 13:00:45 +0300

    Fix:fix the system to allow computer assignment to the library member with valid membership and ensuring the system assign a maximum of one computer to a library member

--- library_management/library_management/doctype/library_computer_assignment/library_computer_assignment.py
+++ library_management/library_management/doctype/library_computer_assignment/library_computer_assignment.py
@@ -1,8 +1,38 @@
 # Copyright (c) 2023, Kabonge and contributors
 # For license information, please see license.txt
 
-# import frappe
+from __future__ import unicode_literals
+
+from frappe import _
+import frappe
 from frappe.model.document import Document
 
 class LibraryComputerAssignment(Document):
-	pass
+    def validate(self):
+        # Check for valid library membership
+        valid_membership = frappe.get_all(
+            'Library Membership',
+            filters={
+                'library_member': self.library_member,
+                'docstatus': 1,
+                'from_date': ('<=', self.assignment_time),
+                'to_date': ('>=', self.assignment_time),
+            },
+            fields=['name']
+        )
+
+        if not valid_membership:
+            frappe.throw(_('The member does not have a valid library membership.'))
+
+        # Ensure that each member is assigned only one computer
+        assigned_computer = frappe.get_all(
+            'Library Computer Assignment',
+            filters={
+                'library_member': self.library_member,
+                'docstatus': 1,
+            },
+            fields=['name']
+        )
+
+        if assigned_computer and assigned_computer[0].name != self.name:
+            frappe.throw(_('This member is already assigned a computer. Only one assignment per member is allowed.'))
